275b4e5ca025dff6eb5eadfd91694c425054b2fc
Louise Ann Apostol <louiseann.apostol@gmail.com>
2019-08-30 21:45:49 +0800

    add styling

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { DiGitPullRequest } from 'react-icons/di';
+import listStyles from './list.module.css';
 
 export type ListProps = {
     heading: string;
@@ -9,9 +11,12 @@ export class List extends React.Component<ListProps> {
     render() {
         const { heading, items } = this.props;
 
-        return (<div>
-            <h3>{heading}</h3>
-            {items.map(item => JSON.stringify(item))}
+        return (<div className={listStyles.list}>
+            <div className={listStyles.listHeading}>
+                <DiGitPullRequest size={'32px'} />
+                <div className={listStyles.listTitle}>{heading}</div>
+            </div>
+            {items.map(item => (<p>{JSON.stringify(item)}</p>))}
         </div>);
     }
 }
