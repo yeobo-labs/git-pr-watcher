5eccef3f483a1dd372c0ef8871674034f609ea91
Louise Ann Apostol <louiseann.apostol@gmail.com>
2019-08-31 19:37:01 +0800

    mock data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 
 import Head from '../components/head';
+import { List } from '../components/list/index';
 
 const IndexPage = () => (
   <>
     <Head title="Git PR Watcher" description="App that listens to Git PR updates" lang="en" />
     <h1>Hello world!</h1>
+    <List heading="List Heading" items={[
+      { id: 1, content: 'a' },
+      { id: 2, content: 'b' }
+    ]} />
   </>
 );
 
