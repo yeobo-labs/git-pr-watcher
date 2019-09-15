c79b82fc77ef015441a55f72bf99623f5811f707
kevin <willardquing@gmail.com>
2019-09-15 16:28:32 +0800

    use store

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,23 +1,26 @@
 import * as React from 'react';
 import { DiGitPullRequest } from 'react-icons/di';
+import { observer, inject } from 'mobx-react';
+import { PRStore } from '../../stores/pull-request-store';
 import Card, { CardProps } from '../card';
 import listStyles from './list.module.css';
 
 export type ListProps = {
     heading: string;
-    items: CardProps[];
 };
 
-export default class List extends React.Component<ListProps> {
+@inject('prStore')
+@observer
+export default class List extends React.Component<ListProps & { prStore: PRStore }> {
     render() {
-        const { heading, items } = this.props;
+        const { heading, prStore: { items } } = this.props;
 
         return (<div className={listStyles.list}>
             <div className={listStyles.listHeading}>
                 <DiGitPullRequest size={'32px'} />
                 <div className={listStyles.listTitle}>{heading}</div>
             </div>
-            {items.map(item => (<Card key={`pr-${item.prNumber}`} {...item} />))}
+            {items.map((item: CardProps) => (<Card key={`pr-${item.prNumber}`} {...item} />))}
         </div>);
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
-import { observer } from 'mobx-react';
-// import * as store from '../stores/pull-request-store';
+import { Provider } from 'mobx-react';
+import { PRStore } from '../stores/pull-request-store';
 import Head from '../components/head';
 import List from '../components/list';
 
-@observer
 class IndexPage extends React.Component {
+  private prStore: PRStore = new PRStore()
+
   render() {
     return (
-      <>
-        <Head title="Git PR Watcher" description="App that listens to Git PR updates" lang="en" />
-        <h1>Hello world!</h1>
-        <List heading="List Heading" items={[]} />
-      </>
+      <Provider prStore={this.prStore}>
+        <div>
+          <Head title="Git PR Watcher" description="App that listens to Git PR updates" lang="en" />
+          <h1>Hello world!</h1>
+          <List heading="List Heading" />
+        </div>
+      </Provider>
     );
   }
 }
