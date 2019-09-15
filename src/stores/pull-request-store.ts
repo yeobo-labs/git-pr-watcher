1bd00e6afef048c26d2d36407b68dc782a065545
kevin <willardquing@gmail.com>
2019-09-15 16:28:24 +0800

    add mock data

--- src/stores/pull-request-store.ts
+++ src/stores/pull-request-store.ts
@@ -1,8 +1,37 @@
+import * as moment from 'moment';
 import { observable, action } from 'mobx';
 import { CardProps } from '../components/card'
 
 export class PRStore {
-  @observable items: CardProps[] = [];
+  @observable items: CardProps[] = [
+    {
+      prNumber: 1,
+      url: 'https://github.com',
+      title: 'PR #1',
+      author: 'author',
+      assignee: 'assignee',
+      status: 'open',
+      date: moment().format('LLL'),
+    },
+    {
+      prNumber: 2,
+      url: 'https://github.com',
+      title: 'PR #1',
+      author: 'author',
+      assignee: 'assignee',
+      status: 'open',
+      date: moment().format('LLL'),
+    },
+    {
+      prNumber: 3,
+      url: 'https://github.com',
+      title: 'PR #1',
+      author: 'author',
+      assignee: 'assignee',
+      status: 'closed',
+      date: moment().format('LLL'),
+    }
+  ];
 
   @action
   add( data: CardProps ) {
