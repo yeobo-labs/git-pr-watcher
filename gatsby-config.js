75ea99993173dfc81b2e7217a30900364c130403
kevin <willardquing@gmail.com>
2019-09-16 12:44:27 +0800

    github gql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,23 @@
+require('dotenv').config({
+  path: '.env',
+})
+
 module.exports = {
   siteMetadata: {
     title: 'Git PR Watcher',
   },
   plugins: [
     'gatsby-plugin-typescript',
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "GitHub",
+        fieldName: "github",
+        url: process.env.GITHUB_API_URL,
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
+        }
+      },
+    },
   ],
 };
