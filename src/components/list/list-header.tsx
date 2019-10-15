1fef5014021767a2ff3de897fdce79ce0a498422
Louise Ann Apostol <louiseann.apostol@gmail.com>
2019-10-15 15:21:20 +0800

    add filter search bar

--- src/components/list/list-header.tsx
+++ src/components/list/list-header.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import { DiGitPullRequest } from 'react-icons/di';
+import { MdSearch} from 'react-icons/md';
 import { ListHeaderProps } from '../../types/list';
 import listStyles from './list.module.css';
 
@@ -9,8 +11,20 @@ export default class ListHeader extends React.Component<ListHeaderProps> {
 
     return (
       <div className={listStyles.listHeading}>
-        <DiGitPullRequest size={'32px'} />
-        <div className={listStyles.listTitle}>{name}</div>
+        <div className={listStyles.listHeadingComponent}>
+          <DiGitPullRequest size={'32px'} />
+          <div className={listStyles.listTitle}>{name}</div>
+        </div>
+        <div className={listStyles.listHeadingComponent}>
+          <div className={classNames(listStyles.listHeadingComponent, listStyles.listFilterContainer)}>
+            <input
+              name="filter"
+              placeholder="Filter list"
+              className={listStyles.listFilterInput}
+            />
+            <MdSearch />
+          </div>
+        </div>
       </div>
     );
   }
