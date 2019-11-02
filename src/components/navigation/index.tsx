f11508c893756889124c59551fb092e02cb9cff6
Louise Ann Apostol <louiseann.apostol@gmail.com>
2019-11-02 18:03:21 +0800

    navbar

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import styles from './navigation.module.css';
 
 const Navigation = () => (
     <div className={styles.container}>
-        <span className={styles.siteName}>GitHub PRoll</span>
+        <ul className={styles.navItems}>
+            <li className={classNames(styles.navItem, styles.navItemLogo)}>
+                <img src="/assets/logo.png" className={styles.logo} />
+            </li>
+            <li className={styles.navItem}>Home</li>
+            <li className={styles.navItem}>Settings</li>
+        </ul>
     </div>
 );
 
